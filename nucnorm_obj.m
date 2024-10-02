65940e2fb0dcafc3e56f772e6dff4a01897fe222
martinmugnier <68703011+martinmugnier@users.noreply.github.com>
2024-10-02 16:33:55 +0200

    Delete nucnorm_obj.m

--- nucnorm_obj.m
+++
@@ -1,6 +0,0 @@
-% nuclear norm objective function
-function [res] = nucnorm_obj(beta,Y,X)
-  [N, TT, K] = size(X);
-  single_index = sum(X.*reshape(kron(beta',ones([N,TT])),N,TT,K), 3);
-  res = norm(svd(Y-single_index),1);
-end
