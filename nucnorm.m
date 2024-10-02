406aa39fd96378c0d85d23ce94b750a3cc905fc4
martinmugnier <68703011+martinmugnier@users.noreply.github.com>
2024-10-02 16:33:48 +0200

    Delete nucnorm.m

--- nucnorm.m
+++
@@ -1,8 +0,0 @@
-% nuclear norm estimator
-function [beta] = nucnorm(Y,X)
-    [N, TT, K] = size(X);
-    beta_init = rand(K,1);
-    beta = fminsearch(@(beta) nucnorm_obj(beta, Y, X), beta_init);
-end
-
-
